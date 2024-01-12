10ba8043aab0d2d75b1c198e179e76f485ac50a4
tj <thinupavani@gmail.com>
2024-01-12 08:57:53 +0530

    opportunities views new

--- src/components/Authenticated/Partner/CreateUpdateModal.jsx
+++ src/components/Authenticated/Partner/CreateUpdateModal.jsx
@@ -51,77 +51,76 @@ export default function CreateUpdateModal({ show, onClose, data, workspaces }) {
 
     return (
         <Transition
-            show={show}
-            enter="transition-opacity duration-75"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="transition-opacity duration-150"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-            className={'w-screen h-screen absolute top-0 left-0 flex items-center justify-center bg-[#0000006d]'}
-        >
-            <div className='bg-white shadow-lg rounded-md h-[90%] lg:h-fit w-[95%] lg:w-[70%]' >
-                <div className='bg-[#C5C5C533] h-14 flex justify-between items-center px-10' >
-                    <div className='font-semibold' >{data ?
-                        <span>View Partner - <span className='text-app-blue-4' >{data?.name}</span></span>
-                        : "Create New Partner"}</div>
-                    <button disabled={loading} onClick={onClose} className='flex justify-center items-center text-app-gray-3' >
-                        <XCircleIcon className='w-7 h-7' />
+        show={show}
+        enter="transition-opacity duration-75"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition-opacity duration-150"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+        className={'w-screen h-screen absolute top-0 left-0 flex items-center justify-center bg-[#0000006d]'}
+    >
+        <div className='bg-white shadow-lg rounded-md h-[90%] lg:h-fit w-[95%] lg:w-[70%]' >
+            <div className='bg-[#C5C5C533] h-14 flex justify-between items-center px-10' >
+                <div className='font-semibold' >{data ?
+                    <span>View Partner - <span className='text-app-blue-4' >{data?.name}</span></span>
+                    : "Create New Partner"}</div>
+                <button disabled={loading} onClick={onClose} className='flex justify-center items-center text-app-gray-3' >
+                    <XCircleIcon className='w-7 h-7' />
+                </button>
+            </div>
+            <div className='max-h-[80vh] h-[80vh] lg:h-fit overflow-scroll no-scrollbar' >
+                <div className='grid gap-5 grid-cols-1 lg:grid-cols-2 px-10 pt-10 pb-5' >
+                    <MainInput
+                        disabled={loading}
+                        value={partner?.name}
+                        onChange={text => setPartner({ ...partner, name: text })}
+                        label={"Partner Name"}
+                        placeholder={"Enter Partner Name"}
+                    />
+                    <MainInput
+                        disabled={loading}
+                        value={partner?.accountName}
+                        onChange={text => setPartner({ ...partner, accountName: text })}
+                        label={"Account Name"}
+                        placeholder={"Enter Account Name"}
+                    />
+                    <MainSelect
+                        disabled={loading}
+                        value={rates?.find(row => row?.name == partner?.rate)}
+                        onChange={value => setPartner({ ...partner, rate: value?.name })}
+                        label={"Rate"}
+                        placeholder={"Please Select Rate"}
+                        options={rates}
+                    />
+                    <MainSelect
+                        disabled={loading}
+                        value={rates?.find(row => row?.name == partner?.rate)}
+                        onChange={value => setPartner({ ...partner, rate: value?.name })}
+                        label={"Rate"}
+                        placeholder={"Please Select Rate"}
+                        options={rates}
+                    />
+                </div>
+                <div className='flex justify-center items-center gap-5 mb-5' >
+                    <button
+                        onClick={onClose}
+                        disabled={loading}
+                        className='disabled:bg-app-gray disabled:border-app-gray disabled:text-white flex items-center gap-3 border text-app-blue-2 border-app-blue-2 rounded-lg w-fit px-10 py-2' >
+                        Cancel
+                    </button>
+                    <button
+                        onClick={() => {
+                            data ? onCreate() : onUpdate()
+                        }}
+                        disabled={loading}
+                        className='disabled:bg-app-gray flex items-center gap-3 bg-app-blue-2 rounded-lg w-fit px-10 py-2 text-white' >
+                        {data ? "Save" : "Create"}
                     </button>
                 </div>
-                <div className='max-h-[80vh] h-[80vh] lg:h-fit overflow-scroll no-scrollbar' >
-                    <div className='grid gap-5 grid-cols-1 lg:grid-cols-2 px-10 pt-10 pb-5' >
-                        <MainInput
-                            disabled={loading}
-                            value={partner?.name}
-                            onChange={text => setPartner({ ...partner, name: text })}
-                            label={"Partner Name"}
-                            placeholder={"Enter Partner Name"}
-                        />
-                        {/* <MainInput
-                            disabled={loading}
-                            value={partner?.accountName}
-                            onChange={text => setPartner({ ...partner, accountName: text })}
-                            label={"Account Name"}
-                            placeholder={"Enter Account Name"}
-                        /> */}
-                        <MainSelect
-                            disabled={loading}
-                            value={workspaces?.find(row => row?.name === partner?.workspaceId)}
-                            onChange={value => setPartner({
-                                ...partner,
-                                workspaceId: value?._id || ''
-                            })}
-                            label={"Workspaces"}
-                            placeholder={"Please Select workspaces"}
-                            options={workspaces}
-                        />
-                        {/* <MainSelect
-                            disabled={loading}
-                            value={rates?.find(row => row?.name == partner?.rate)}
-                            onChange={value => setPartner({ ...partner, rate: value?.name })}
-                            label={"Rate"}
-                            placeholder={"Please Select Rate"}
-                            options={rates}
-                        /> */}
-                    </div>
-                    <div className='flex justify-center items-center gap-5 mb-5' >
-                        <button
-                            onClick={onClose}
-                            disabled={loading}
-                            className='disabled:bg-app-gray disabled:border-app-gray disabled:text-white flex items-center gap-3 border text-app-blue-2 border-app-blue-2 rounded-lg w-fit px-10 py-2' >
-                            Cancel
-                        </button>
-                        <button
-                            onClick={onCreate}
-                            disabled={loading}
-                            className='disabled:bg-app-gray flex items-center gap-3 bg-app-blue-2 rounded-lg w-fit px-10 py-2 text-white' >
-                            {data ? "Save" : "Create"}
-                        </button>
-                    </div>
-                </div>
-
             </div>
-        </Transition> 
+
+        </div>
+    </Transition>
     )
 }
